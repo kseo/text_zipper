a4d296abe5bca89297e50072003e15de87443010
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-12-12 13:15:13 +0900

    Initial import.

---
+++ README.md
@@ -0,0 +1,19 @@
+# text_zipper
+
+A library for Dart developers. It is awesome.
+
+## Usage
+
+A simple usage example:
+
+    import 'package:text_zipper/text_zipper.dart';
+
+    main() {
+      var awesome = new Awesome();
+    }
+
+## Features and bugs
+
+Please file feature requests and bugs at the [issue tracker][tracker].
+
+[tracker]: http://example.com/issues/replaceme
---
+++ lib/text_zipper.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The text_zipper library.
+///
+/// This is an awesome library. More dartdocs go here.
+library text_zipper;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/text_zipper_base.dart';
