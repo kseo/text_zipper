b22835e131240f7e7c4aa1ddd478c54ae44781f3
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-12-14 22:07:57 +0900

    Add an example program.

--- example/text_zipper_example.dart
+++ example/text_zipper_example.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, Kwang Yul Seo. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library text_zipper.example;
@@ -6,6 +6,16 @@ library text_zipper.example;
 import 'package:text_zipper/text_zipper.dart';
 
 main() {
-  var awesome = new Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  const String text = '''
+abc
+df
+efg''';
+
+  var tz = stringZipper(text);
+  tz = tz.moveCursorTo(new Position(1, 1)).insertCharCode('e'.codeUnitAt(0));
+  print(tz.text);
+  /// abc
+  /// def
+  /// efg
 }
+
